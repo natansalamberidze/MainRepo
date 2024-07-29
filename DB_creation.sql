1abd5373a481a8ba81d6cf09126425a214637965
Natans Šalamberidze <103418347+natansalamberidze@users.noreply.github.com>
2024-07-29 16:00:04 +0300

    Add files via upload

---
+++ DB_creation.sql
@@ -0,0 +1,15 @@
+insert into j_biblioteka values (SYS_GUID(), CURRENT_DATE, '
+   [
+  {
+    "@id": "1",
+    "nosaukums": "Ievads XML",
+    "autors": "Janis Kalnins",
+    "izdosanas_gads": "2020",
+    "izdevuma_valoda": "Latviesu",
+    "izdevuma veids": "Gramata",
+    "izdevuma apskats": {
+      "apraksts": "Si gramata piedava ieskatu XML pamatos un tas pielietojuma web izstrade.", 
+      "vertejums": "4.5"
+    }
+   },
+   {
